448dcbf3f79626d70fc7f9875dee7a8ab103fee4
Jeff Higgason <jhiggy@gmail.com>
2023-11-29 17:34:17 -0800

    fixed copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Common Robotics Platform - PARTS'
-copyright = '2023, Jeff Higgason'
+copyright = '2023, Portland Area Robotics Society'
 author = 'Jeff Higgason'
 
 # -- General configuration ---------------------------------------------------
